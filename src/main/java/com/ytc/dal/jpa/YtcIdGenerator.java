e75693b61519583ac402052e1863471bc337bdba
Sheik Ameer <munavars@CA.NA.YTC>
2017-04-26 00:31:45 -0700

    BaseDao changes for create

---
+++ src/main/java/com/ytc/dal/jpa/YtcIdGenerator.java
@@ -0,0 +1,13 @@
+package com.ytc.dal.jpa;
+
+import java.io.Serializable;
+
+import org.hibernate.engine.spi.SessionImplementor;
+
+public class YtcIdGenerator extends org.hibernate.id.IncrementGenerator {
+    @Override
+    public Serializable generate(SessionImplementor session, Object object) {
+        Serializable id = session.getEntityPersister(null, object).getClassMetadata().getIdentifier(object, session);
+        return (id != null && !"".equals(id.toString().trim())) ? id : ((Integer) super.generate(session, object));
+    }
+}
--- src/main/java/com/ytc/dal/model/DalModel.java
+++ src/main/java/com/ytc/dal/model/DalModel.java
@@ -2,9 +2,11 @@ package com.ytc.dal.model;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 @MappedSuperclass
 public abstract class DalModel implements Cloneable {
@@ -22,8 +24,9 @@ public abstract class DalModel implements Cloneable {
 
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
+	@Column(name = "ID")
+	@GeneratedValue(generator = "YtcIdGenerator")
+	@GenericGenerator(name = "YtcIdGenerator", strategy = "com.ytc.dal.jpa.YtcIdGenerator", parameters = { @Parameter(name = "strategy", value = "uuid") })
 	public Integer getId() { 
 		return id;
 	}

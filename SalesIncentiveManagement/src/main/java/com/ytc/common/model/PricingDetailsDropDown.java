02cc92c0f652e6a1b859b6c961a55cf629ed26fe
ArunPurushothaman <arun.purushothaman@yokohamatire.com>
2017-06-08 23:50:00 -0700

    Pricing form changes

---
+++ SalesIncentiveManagement/src/main/java/com/ytc/common/model/PricingDetailsDropDown.java
@@ -0,0 +1,130 @@
+/**
+ * 
+ */
+package com.ytc.common.model;
+
+import java.util.List;
+
+/**
+ * @author ArunP
+ *
+ */
+public class PricingDetailsDropDown {
+	private List<DropDown> groupList;
+	private List<DropDown> custBillToList;
+	private List<DropDown> custByNameList;
+	private List<DropDown> custShipToList;
+	private List<DropDown> termsCodeList;
+	private List<DropDown> otherShipReqsList;
+	private List<DropDown> definitionList;
+	private List<DropDown> dueDatesList;
+	private List<DropDown> cashList;
+	/**
+	 * @return the groupList
+	 */
+	public List<DropDown> getGroupList() {
+		return groupList;
+	}
+	/**
+	 * @param groupList the groupList to set
+	 */
+	public void setGroupList(List<DropDown> groupList) {
+		this.groupList = groupList;
+	}
+	/**
+	 * @return the custBillToList
+	 */
+	public List<DropDown> getCustBillToList() {
+		return custBillToList;
+	}
+	/**
+	 * @param custBillToList the custBillToList to set
+	 */
+	public void setCustBillToList(List<DropDown> custBillToList) {
+		this.custBillToList = custBillToList;
+	}
+	/**
+	 * @return the custByNameList
+	 */
+	public List<DropDown> getCustByNameList() {
+		return custByNameList;
+	}
+	/**
+	 * @param custByNameList the custByNameList to set
+	 */
+	public void setCustByNameList(List<DropDown> custByNameList) {
+		this.custByNameList = custByNameList;
+	}
+	/**
+	 * @return the custShipToList
+	 */
+	public List<DropDown> getCustShipToList() {
+		return custShipToList;
+	}
+	/**
+	 * @param custShipToList the custShipToList to set
+	 */
+	public void setCustShipToList(List<DropDown> custShipToList) {
+		this.custShipToList = custShipToList;
+	}
+	/**
+	 * @return the termsCodeList
+	 */
+	public List<DropDown> getTermsCodeList() {
+		return termsCodeList;
+	}
+	/**
+	 * @param termsCodeList the termsCodeList to set
+	 */
+	public void setTermsCodeList(List<DropDown> termsCodeList) {
+		this.termsCodeList = termsCodeList;
+	}
+	/**
+	 * @return the otherShipReqsList
+	 */
+	public List<DropDown> getOtherShipReqsList() {
+		return otherShipReqsList;
+	}
+	/**
+	 * @param otherShipReqsList the otherShipReqsList to set
+	 */
+	public void setOtherShipReqsList(List<DropDown> otherShipReqsList) {
+		this.otherShipReqsList = otherShipReqsList;
+	}
+	/**
+	 * @return the definitionList
+	 */
+	public List<DropDown> getDefinitionList() {
+		return definitionList;
+	}
+	/**
+	 * @param definitionList the definitionList to set
+	 */
+	public void setDefinitionList(List<DropDown> definitionList) {
+		this.definitionList = definitionList;
+	}
+	/**
+	 * @return the dueDatesList
+	 */
+	public List<DropDown> getDueDatesList() {
+		return dueDatesList;
+	}
+	/**
+	 * @param dueDatesList the dueDatesList to set
+	 */
+	public void setDueDatesList(List<DropDown> dueDatesList) {
+		this.dueDatesList = dueDatesList;
+	}
+	/**
+	 * @return the cashList
+	 */
+	public List<DropDown> getCashList() {
+		return cashList;
+	}
+	/**
+	 * @param cashList the cashList to set
+	 */
+	public void setCashList(List<DropDown> cashList) {
+		this.cashList = cashList;
+	}
+}
---
+++ SalesIncentiveManagement/src/main/java/com/ytc/service/IPricingUpdateService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.ytc.service;
+
+import com.ytc.common.model.PricingHeader;
+
+/**
+ * @author ArunP
+ *
+ */
+public interface IPricingUpdateService {
+
+	
+	PricingHeader savePricingHeaderDetails(PricingHeader pricingHeader); 
+}

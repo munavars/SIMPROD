c9748117cd0346158a1883058a418670caef5657
Sheik Ameer <munavars@CA.NA.YTC>
2017-06-13 05:20:43 -0700

    Pricing form customer Id validation

--- src/main/java/com/ytc/service/impl/PricingUpdateServiceImpl.java
+++ src/main/java/com/ytc/service/impl/PricingUpdateServiceImpl.java
@@ -17,7 +17,6 @@ import org.springframework.util.StringUtils;
 import com.ytc.common.model.PricingDetail;
 import com.ytc.common.model.PricingHeader;
 import com.ytc.dal.IDataAccessLayer;
-import com.ytc.dal.model.DalCustomer;
 import com.ytc.dal.model.DalPricingCustomerType;
 import com.ytc.dal.model.DalPricingDetail;
 import com.ytc.dal.model.DalPricingHeader;
@@ -38,125 +37,125 @@ public class PricingUpdateServiceImpl implements IPricingUpdateService{
 
 	@Override
 	public PricingHeader savePricingHeaderDetails(PricingHeader pricingHeader) {
-		
+
 		/*if(pricingHeader.getId() != null && pricingHeader.getPricingDetailList().get(0).getId() != null){
-			
+
 			DalPricingDetail dalPricingDetail = baseDao.getById(DalPricingDetail.class, pricingHeader.getPricingDetailList().get(0).getId());
 		}*/
-			try {
-				if(pricingHeader != null ){
-					createPricingDetails(pricingHeader);
-				}
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				LOGGER.info("Error in saving Pricing Header Details" +e);
+		try {
+			if(pricingHeader != null ){
+				createPricingDetails(pricingHeader);
 			}
-				
-		
-	
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			LOGGER.info("Error in saving Pricing Header Details" +e);
+		}
+
+
+
 		return pricingHeader;
 	}
 	@Transactional
-private void createPricingDetails(PricingHeader pricingHeader) {
-	DalPricingHeader dalPricingHeader = null;
-	if(pricingHeader != null ){
-		if(validateCustomerId(pricingHeader.getCustomerId()) && validatePartProdTread(pricingHeader.getPricingDetailList())){
-		dalPricingHeader = new DalPricingHeader();	
-		dalPricingHeader.setCustomerId(Integer.valueOf(pricingHeader.getCustomerId()));
-		Map<String, Object> parameters = new HashMap<String, Object>();
-		parameters.put("customerType", pricingHeader.getCustomerType());
-		List<DalPricingCustomerType> dalPricingCustomerTypeList =baseDao.getByType("DalPricingCustomerType.getCustomerType", parameters);
-		for(DalPricingCustomerType dalPricingCustomerType:dalPricingCustomerTypeList){
-		dalPricingHeader.setCustomerType(dalPricingCustomerType);
-		parameters.remove("customerType", pricingHeader.getCustomerType());
-		}
-	parameters.put("code", pricingHeader.getTermCode());
-		List<DalPricingTermCodes> dalPricingTermCodesList =baseDao.getByType("DalPricingTermCodes.getCode", parameters);
-		for(DalPricingTermCodes dalPricingTermCodes:dalPricingTermCodesList){
-		dalPricingHeader.setTermCodes(dalPricingTermCodes);
-		parameters.remove("code", pricingHeader.getTermCode());
-		}
-		parameters.put("shipRqs", pricingHeader.getShippingReqs());
-		List<DalPricingShipRequirements> dalPricingShipRequirementsList =baseDao.getByType("DalPricingShipRequirements.getShipRqs", parameters);
-		for(DalPricingShipRequirements dalPricingShipRequirements:dalPricingShipRequirementsList){
-		dalPricingHeader.setShippingReqs(dalPricingShipRequirements);
-		parameters.remove("shipRqs", pricingHeader.getShippingReqs());
-		}
-		
-		parameters.put("otherReqs", pricingHeader.getOtherShippingReqs());
-		List<DalPricingOtherShipRequirements> dalPricingOtherShipRequirementsList =baseDao.getByType("DalPricingOtherShipRequirements.getOtherReq", parameters);
-		for(DalPricingOtherShipRequirements dalPricingOtherShipRequirements:dalPricingOtherShipRequirementsList){
-		dalPricingHeader.setOtherShippingreqs(dalPricingOtherShipRequirements);
-		parameters.remove("otherReqs", pricingHeader.getOtherShippingReqs());
-		}
-		dalPricingHeader.setUserComments(StringUtils.isEmpty(pricingHeader.getUserComments())  ? "": pricingHeader.getUserComments() );
-	
-		
-		
-		
-		
-	
-		
-		/** Save pricing Detail section information*/
-		List<DalPricingDetail> dalPricingDetailsList = createPricingDetailsData(pricingHeader,dalPricingHeader);
-		
-		if(!dalPricingDetailsList.isEmpty()){
-			 dalPricingHeader.setDalPricingDetailList(dalPricingDetailsList);
-			 
-		 }
-		
-		baseDao.merge(dalPricingHeader);
-		
-			pricingHeader.setSuccess(true);
-		}
-	}		
-}
-	
-	
+	private void createPricingDetails(PricingHeader pricingHeader) {
+		DalPricingHeader dalPricingHeader = null;
+		if(pricingHeader != null ){
+			if(validateCustomerId(pricingHeader) && validatePartProdTread(pricingHeader.getPricingDetailList())){
+				dalPricingHeader = new DalPricingHeader();	
+				dalPricingHeader.setCustomerId(Integer.valueOf(pricingHeader.getCustomerId()));
+				Map<String, Object> parameters = new HashMap<String, Object>();
+				parameters.put("customerType", pricingHeader.getCustomerType());
+				List<DalPricingCustomerType> dalPricingCustomerTypeList =baseDao.getByType("DalPricingCustomerType.getCustomerType", parameters);
+				for(DalPricingCustomerType dalPricingCustomerType:dalPricingCustomerTypeList){
+					dalPricingHeader.setCustomerType(dalPricingCustomerType);
+					parameters.remove("customerType", pricingHeader.getCustomerType());
+				}
+				parameters.put("code", pricingHeader.getTermCode());
+				List<DalPricingTermCodes> dalPricingTermCodesList =baseDao.getByType("DalPricingTermCodes.getCode", parameters);
+				for(DalPricingTermCodes dalPricingTermCodes:dalPricingTermCodesList){
+					dalPricingHeader.setTermCodes(dalPricingTermCodes);
+					parameters.remove("code", pricingHeader.getTermCode());
+				}
+				parameters.put("shipRqs", pricingHeader.getShippingReqs());
+				List<DalPricingShipRequirements> dalPricingShipRequirementsList =baseDao.getByType("DalPricingShipRequirements.getShipRqs", parameters);
+				for(DalPricingShipRequirements dalPricingShipRequirements:dalPricingShipRequirementsList){
+					dalPricingHeader.setShippingReqs(dalPricingShipRequirements);
+					parameters.remove("shipRqs", pricingHeader.getShippingReqs());
+				}
+
+				parameters.put("otherReqs", pricingHeader.getOtherShippingReqs());
+				List<DalPricingOtherShipRequirements> dalPricingOtherShipRequirementsList =baseDao.getByType("DalPricingOtherShipRequirements.getOtherReq", parameters);
+				for(DalPricingOtherShipRequirements dalPricingOtherShipRequirements:dalPricingOtherShipRequirementsList){
+					dalPricingHeader.setOtherShippingreqs(dalPricingOtherShipRequirements);
+					parameters.remove("otherReqs", pricingHeader.getOtherShippingReqs());
+				}
+				dalPricingHeader.setUserComments(StringUtils.isEmpty(pricingHeader.getUserComments())  ? "": pricingHeader.getUserComments() );
+
+
+
+
+
+
+
+				/** Save pricing Detail section information*/
+				List<DalPricingDetail> dalPricingDetailsList = createPricingDetailsData(pricingHeader,dalPricingHeader);
+
+				if(!dalPricingDetailsList.isEmpty()){
+					dalPricingHeader.setDalPricingDetailList(dalPricingDetailsList);
+
+				}
+
+				baseDao.merge(dalPricingHeader);
+
+				pricingHeader.setSuccess(true);
+			}
+		}		
+	}
+
+
 	private List<DalPricingDetail> createPricingDetailsData( PricingHeader pricingHeader,DalPricingHeader dalPricingHeader) {
-		
+
 		List<DalPricingDetail> dalPricingDetailsList = new ArrayList<DalPricingDetail>();
 		DalPricingDetail dalPricingDetail = null;
-		
+
 		for(PricingDetail pricingDetail: pricingHeader.getPricingDetailList() ){
-		 dalPricingDetail = new DalPricingDetail();
-		if(pricingDetail != null ){
-			dalPricingDetail.setAddChgDel(pricingDetail.getAddChangeDel());
-			//dalPricingDetail.setDiscType(pricingDetail.getComments());
-			Calendar cal = Calendar.getInstance();
-			cal.setTimeInMillis(pricingDetail.getBeginDate().getTime());
-			dalPricingDetail.setStartDate(cal);
-			Calendar cal1 = Calendar.getInstance();
-			cal1.setTimeInMillis(pricingDetail.getEndDate().getTime());
-			dalPricingDetail.setEndDate(cal1);
-			if(!StringUtils.isEmpty(pricingDetail.getInvoiceDisc())){
-			dalPricingDetail.setInvoiceDisc(Double.valueOf(trimPercentage(pricingDetail.getInvoiceDisc())));
+			dalPricingDetail = new DalPricingDetail();
+			if(pricingDetail != null ){
+				dalPricingDetail.setAddChgDel(pricingDetail.getAddChangeDel());
+				//dalPricingDetail.setDiscType(pricingDetail.getComments());
+				Calendar cal = Calendar.getInstance();
+				cal.setTimeInMillis(pricingDetail.getBeginDate().getTime());
+				dalPricingDetail.setStartDate(cal);
+				Calendar cal1 = Calendar.getInstance();
+				cal1.setTimeInMillis(pricingDetail.getEndDate().getTime());
+				dalPricingDetail.setEndDate(cal1);
+				if(!StringUtils.isEmpty(pricingDetail.getInvoiceDisc())){
+					dalPricingDetail.setInvoiceDisc(Double.valueOf(trimPercentage(pricingDetail.getInvoiceDisc())));
+				}
+				if(!StringUtils.isEmpty(pricingDetail.getNetPrice())){
+					dalPricingDetail.setNetPrice(Double.valueOf(pricingDetail.getNetPrice()));
+				}
+
+				dalPricingDetail.setIsBonusableUnits(pricingDetail.getBonusUnits());
+				dalPricingDetail.setIsCommissionable(pricingDetail.getCommissionable());
+				dalPricingDetail.setPartNumber(pricingDetail.getPart());
+				dalPricingDetail.setProdLine(pricingDetail.getProdLine());
+				dalPricingDetail.setProdTread(pricingDetail.getTread());
+				dalPricingDetail.setDalPricingHeader(dalPricingHeader);
 			}
-			if(!StringUtils.isEmpty(pricingDetail.getNetPrice())){
-			dalPricingDetail.setNetPrice(Double.valueOf(pricingDetail.getNetPrice()));
-			}
-			
-			dalPricingDetail.setIsBonusableUnits(pricingDetail.getBonusUnits());
-			dalPricingDetail.setIsCommissionable(pricingDetail.getCommissionable());
-			dalPricingDetail.setPartNumber(pricingDetail.getPart());
-			dalPricingDetail.setProdLine(pricingDetail.getProdLine());
-			dalPricingDetail.setProdTread(pricingDetail.getTread());
-			 dalPricingDetail.setDalPricingHeader(dalPricingHeader);
+
+			dalPricingDetailsList.add(dalPricingDetail);
 		}
-		
-		 dalPricingDetailsList.add(dalPricingDetail);
-		}
-		
+
 		return dalPricingDetailsList;
-		}
+	}
 	public String trimPercentage(String str) {
 		if(!StringUtils.isEmpty(str)){
-	    if (str.charAt(str.length()-1)=='%'){
-	        str = str.replace(str.substring(str.length()-1), "");
-	        return str;
-	    } else{
-	        return str;
-	    }
+			if (str.charAt(str.length()-1)=='%'){
+				str = str.replace(str.substring(str.length()-1), "");
+				return str;
+			} else{
+				return str;
+			}
 		}
 		return str;
 	}
@@ -166,8 +165,8 @@ private void createPricingDetails(PricingHeader pricingHeader) {
 		List<String> prodLineValidated=null;
 		List<String> prodTreadValidated=null;
 		boolean validatePart = false;
-		 boolean validateProdLine = false;
-		 boolean validateProdTread = false;
+		boolean validateProdLine = false;
+		boolean validateProdTread = false;
 		List<String> partNumber = new ArrayList<String>();
 		List<String> prodLine = new ArrayList<String>();
 		List<String> prodTread = new ArrayList<String>();
@@ -175,60 +174,70 @@ private void createPricingDetails(PricingHeader pricingHeader) {
 		Map<String, Object> queryProdLine= new HashMap<>();	
 		Map<String, Object> queryTread = new HashMap<>();	
 		for(PricingDetail pricingDetail: pricingDetailsList ){
-				if(!StringUtils.isEmpty(pricingDetail.getPart()) && !StringUtils.isEmpty(pricingDetail.getProdLine()) && !StringUtils.isEmpty(pricingDetail.getTread())){
+			if(!StringUtils.isEmpty(pricingDetail.getPart()) && !StringUtils.isEmpty(pricingDetail.getProdLine()) && !StringUtils.isEmpty(pricingDetail.getTread())){
 				partNumber.add(pricingDetail.getPart());
 				queryPart.put("partNumber", partNumber);
-				 partValidated= baseDao.getListFromNamedQueryWithParameter("DalPartMaster.getPartNumber",queryPart);
-				 validatePart= arraysMatch(partNumber, partValidated);
+				partValidated= baseDao.getListFromNamedQueryWithParameter("DalPartMaster.getPartNumber",queryPart);
+				validatePart= arraysMatch(partNumber, partValidated);
 				prodLine.add(pricingDetail.getProdLine());
 				queryProdLine.put("productLine", prodLine);
-				 prodLineValidated= baseDao.getListFromNamedQueryWithParameter("DalPartMaster.getProductLine",queryProdLine);
-				 validateProdLine = arraysMatch(prodLine, prodLineValidated);
+				prodLineValidated= baseDao.getListFromNamedQueryWithParameter("DalPartMaster.getProductLine",queryProdLine);
+				validateProdLine = arraysMatch(prodLine, prodLineValidated);
 				prodTread.add(pricingDetail.getTread());
 				queryTread.put("treadDesc", prodTread);
 				prodTreadValidated= baseDao.getListFromNamedQueryWithParameter("DalPartMaster.getTreadDesc",queryTread);
-				 validateProdTread = arraysMatch(prodTread, prodTreadValidated);
-		if(validatePart && validateProdLine && validateProdTread){
-			isValidated = true;
-		}
-				}else{
-					isValidated = false;
-					break;
+				validateProdTread = arraysMatch(prodTread, prodTreadValidated);
+				if(validatePart && validateProdLine && validateProdTread){
+					isValidated = true;
 				}
-				
-	}
-		
-		return isValidated;
-		
+			}else{
+				isValidated = false;
+				break;
+			}
+
 		}
+
+		return isValidated;
+
+	}
 	public boolean arraysMatch(List<String> partNumber, List<String> partValidated) {
-	   int count = 0;
-	    List<String> match = new ArrayList(partValidated);
-	    if(!partNumber.isEmpty()){
-	    for (String element : partNumber) {
-	        if (match.contains(element)) {
-	            count++;
-	        }
-	    }
-	    }
-	    if(partNumber.size() == count){
-	    return true;
-	    }
+		int count = 0;
+		List<String> match = new ArrayList(partValidated);
+		if(!partNumber.isEmpty()){
+			for (String element : partNumber) {
+				if (match.contains(element)) {
+					count++;
+				}
+			}
+		}
+		if(partNumber.size() == count){
+			return true;
+		}
 		return false;
 	}
-	private boolean validateCustomerId(String custId){
+	
+	
+	private boolean validateCustomerId(PricingHeader pricingHeader){
 		boolean validate = false;
+		String custId=pricingHeader.getCustomerId();
 		List<String> dalCustomer = null;
-		Map<String, Object> queryParam = new HashMap<>();	
-				if(!StringUtils.isEmpty(custId)){
-					queryParam.put("customerNumber", custId);
-					dalCustomer= baseDao.getByType("DalCustomer.getCustomerId",queryParam);
-					if(!StringUtils.isEmpty(dalCustomer.get(0))){
-						validate = true;
-					}
-				}
+		Map<String, Object> queryParam = new HashMap<>();    
+		if(!StringUtils.isEmpty(custId)){
+			queryParam.put("customerNumber", custId);
+			dalCustomer= baseDao.getByType("DalCustomer.getCustomerId",queryParam);
+			/*if(!StringUtils.isEmpty(dalCustomer.get(0))){
+                                   validate = true;
+                            }*/
+			if(!dalCustomer.isEmpty()){
+				validate = true;
+			}
+		}
+		/* if(!validate){
+                            pricingHeader.setValidationMessage("Entered Customer Id is not valid");     
+                     }*/
 		return validate;
-		
-		
+
+
 	}
+
 }

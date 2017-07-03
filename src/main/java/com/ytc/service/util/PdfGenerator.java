2a5b0b0b29f3a6992d11e75e0ce110229b8d05d0
manikandans47 <manikandan.subramaniam@yokohamatire.com>
2017-07-02 21:00:04 -0700

    Defect fixes.

--- src/main/java/com/ytc/service/util/PdfGenerator.java
+++ src/main/java/com/ytc/service/util/PdfGenerator.java
@@ -275,6 +275,7 @@ public class PdfGenerator {
 		         cell1.setVerticalAlignment(Element.ALIGN_MIDDLE);
 		         table.addCell(cell1);	 
 	         }
+
 	         DecimalFormat format1 = new DecimalFormat("#,###.00");
 	         if(ProgramConstant.ZERO.equalsIgnoreCase(dalpgm.getIsTiered())){
 	        	 if(dalpgm.getAccrualAmount()==0)	{
@@ -284,8 +285,18 @@ public class PdfGenerator {
 	        	 }
 
 	         }else{
-	        	 cell1 = new PdfPCell(new Paragraph("Amount: "));	 
+	        	 if(!ProgramConstant.CALCULATED_PROGRAM_TYPE.equalsIgnoreCase(dalpgm.getDalProgramType().getType())){
+		        	 if(dalpgm.getAccrualAmount()==0)	{
+		        		 cell1 = new PdfPCell(new Paragraph("Amount: 0"+dalpgm.getAccrualType()));
+		        	 }else{
+		    	         cell1 = new PdfPCell(new Paragraph("Amount: "+ format1.format(dalpgm.getAccrualAmount())+dalpgm.getAccrualType()));
+		        	 }
+	        	 }
+	        	 else{
+	        		 cell1 = new PdfPCell(new Paragraph("Amount: "));	 
+	        	 }
 	         }
+
 	         cell1.setBorderColor(BaseColor.BLACK);
 	         cell1.setPaddingLeft(10);
 	         //cell6.setHorizontalAlignment(Element.ALIGN_CENTER);

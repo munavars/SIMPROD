28fbd575661e6fa98d2d52c1fdee78ce614cc8f0
ArunPurushothaman <arun.purushothaman@yokohamatire.com>
2017-07-11 23:39:16 -0700

    CCM change

--- src/main/java/com/ytc/service/ICcmService.java
+++ src/main/java/com/ytc/service/ICcmService.java
@@ -2,6 +2,7 @@ package com.ytc.service;
 
 import java.util.List;
 
+import com.ytc.common.model.CcmDetails;
 import com.ytc.common.model.DropDown;
 
 public interface ICcmService {
@@ -9,4 +10,6 @@ public interface ICcmService {
 	List<DropDown> getFrequencyDropDownList(String namedQueryValue);
 
 	boolean createMemoData(Integer id);
+	
+	List<CcmDetails> getCCMDetails(String frequency, String bu, String beginDate, String endDate);
 }
